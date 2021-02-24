9b1a9da1144d573568f3db826ef6ee27045147bf
Bobby <bobby.smith214@gmail.com>
2021-02-23 21:04:34 -0800

    Day1 solution

---
+++ Day1/Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+
+int main()
+{
+  int i         = 4;
+  double d      = 4.0;
+  std::string s = "HackerRank ";
+  int i2;
+  double d2;
+  std::string s2;
+
+  std::cin >> i2;
+  std::cin >> d2;
+  std::cin.ignore();
+  std::getline(std::cin, s2);
+  
+  std::cout << i + i2 << std::endl;
+  std::cout << std::fixed << std::setprecision(1) << d + d2 << std::endl;
+  std::cout << s + s2 << std::endl;
+
+  //system("pause");
+  return 0;
+}
